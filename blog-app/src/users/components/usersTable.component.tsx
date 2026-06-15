bf71587a23a6e2dee30d3283f9f396bc567b5820
Michal <micad132132@gmail.com>
2026-06-15 22:50:55 +0200

    improved users table a little bit

--- blog-app/src/users/components/usersTable.component.tsx
+++ blog-app/src/users/components/usersTable.component.tsx
@@ -1,11 +1,19 @@
-import { Table } from "@chakra-ui/react"
+import { Button, Table } from "@chakra-ui/react"
 import type { UserResponseDTO } from "../../types/userTypes.ts";
 import styled from "styled-components";
+import RoleBadgeComponent from "../../components/roleBadge.component.tsx";
 
 
 const TableWrapper = styled.div`
     max-width: 1200px;
     margin: 20px auto;
+`
+
+
+const TableRow = styled(Table.Row)`
+`
+
+const TableCell = styled(Table.Cell)`
 `
 
 interface Props {
@@ -16,23 +24,39 @@ const UsersTableComponent = ({ users }: Props) => {
 
     return (
         <TableWrapper>
-        <Table.Root size="sm">
+        <Table.Root
+            size="sm"
+            striped
+        >
             <Table.Header>
                 <Table.Row>
                     <Table.ColumnHeader>Id:</Table.ColumnHeader>
                     <Table.ColumnHeader>Username:</Table.ColumnHeader>
                     <Table.ColumnHeader>City:</Table.ColumnHeader>
                     <Table.ColumnHeader>Country:</Table.ColumnHeader>
+                    <Table.ColumnHeader>Role:</Table.ColumnHeader>
+                    <Table.ColumnHeader>Action:</Table.ColumnHeader>
                 </Table.Row>
             </Table.Header>
             <Table.Body>
                 {users.map((user) => (
-                    <Table.Row key={user.id}>
-                        <Table.Cell>{user.id}</Table.Cell>
-                        <Table.Cell>{user.username}</Table.Cell>
-                        <Table.Cell>{user.city}</Table.Cell>
-                        <Table.Cell>{user.country}</Table.Cell>
-                    </Table.Row>
+                    <TableRow key={user.id}>
+                        <TableCell>{user.id}</TableCell>
+                        <TableCell>{user.username}</TableCell>
+                        <TableCell>{user.city}</TableCell>
+                        <TableCell>{user.country}</TableCell>
+                        <TableCell><RoleBadgeComponent role={user.role} /></TableCell>
+                        <TableCell>
+
+                            <Button
+                                variant="solid"
+                                colorPalette="red"
+                                size="xs"
+                            >
+                                Delete
+                            </Button>
+                        </TableCell>
+                    </TableRow>
                 ))}
             </Table.Body>
         </Table.Root>
